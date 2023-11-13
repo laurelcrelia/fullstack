b3e7bf9536878706ba0be6065411e0699c6db946
laurelcrelia <laurel.matt@helsinki.fi>
2023-11-13 20:51:25 +0200

    1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -9,27 +9,37 @@ const Header = (props) => {
   )
 }
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
+
+const Percentage = (props) => {
+  console.log(props)
+  return <p> positive {(100 * props.good) / props.all} %</p>
+} 
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
   return (
     <div>
       <Header header="give feedback" />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => setGood(good + 1) + setAll(all + 1) + setAverage(average + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1) + setAll(all + 1) + setAverage(average + 0)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1) + setAll(all + 1)+ setAverage(average -1)} text="bad" />
       <Header header="statistics" />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average/all}</p>
+      <Percentage all={all} good={good} />
     </div>
   )
 }
