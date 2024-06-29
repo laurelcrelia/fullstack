bff8fb3a7343a95dee06b481d058c32aad85c20e
laurelcrelia <laurel.matt@helsinki.fi>
2024-06-29 22:41:28 +0300

    4.5

--- osa4/tests/list_helper.test.js
+++ osa4/tests/list_helper.test.js
@@ -2,6 +2,53 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const listWithoutBlogs = []
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+const listWithManyBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f5',
+    title: 'Testiblogi 1',
+    author: 'Aapo',
+    url: 'http://www.aaponblogi.fi',
+    likes: 1,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f6',
+    title: 'Testiblogi 2',
+    author: 'Bertta',
+    url: 'http://www.bertanblogi.fi',
+    likes: 2,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f7',
+    title: 'Testiblogi 3',
+    author: 'Cecilia',
+    url: 'http://www.cecilianblogi.fi',
+    likes: 3,
+    __v: 0
+  },
+  {
+    _id: '4a422aa71b54a676234d17f8',
+    title: 'Testiblogi 4',
+    author: 'Daniel',
+    url: 'http://www.danielinblogi.fi',
+    likes: 3,
+    __v: 0
+  }
+]
+
 describe('dummy', () => {
   test('dummy returns one', () => {
     const blogs = []
@@ -12,42 +59,6 @@ describe('dummy', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-  const listWithManyBlogs = [
-    {
-      _id: '5a422aa71b54a676234d17f5',
-      title: 'Testiblogi 1',
-      author: 'Aapo',
-      url: 'http://www.aaponblogi.fi',
-      likes: 1,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f6',
-      title: 'Testiblogi 2',
-      author: 'Bertta',
-      url: 'http://www.bertanblogi.fi',
-      likes: 2,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f7',
-      title: 'Testiblogi 3',
-      author: 'Cecilia',
-      url: 'http://www.cecilianblogi.fi',
-      likes: 3,
-      __v: 0
-    }
-  ]
   test('of empty list is zero', () => {
     const blogs = []
 
@@ -60,6 +71,29 @@ describe('total likes', () => {
   })
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
-    assert.strictEqual(result, 6)
+    assert.strictEqual(result, 9)
+  })
+})
+
+describe('favorite blog', () => {
+  test('is calculated right when no blogs', () => {
+    const result = listHelper.favoriteBlog(listWithoutBlogs)
+    assert.strictEqual(result, null)
+  })
+  test('is calculated right when one blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepEqual(result,  {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    })
+  })
+  test('returns first of the favorite blogs when there are multiple with same amount of likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    assert.deepEqual(result,  {
+      title: 'Testiblogi 3',
+      author: 'Cecilia',
+      likes: 3,
+    })
   })
 })
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,6 +6,25 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) {
+    return null
+  }
+  else {
+    let favorite = blogs[0]
+    blogs.forEach((blog) => {
+      if (blog.likes > favorite.likes) {
+        favorite = blog
+      }
+    })
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
