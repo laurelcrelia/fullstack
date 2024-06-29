02fd028641f59318a8c425b9ad9b3aa72e765a88
laurelcrelia <laurel.matt@helsinki.fi>
2024-06-29 14:56:51 +0300

    4.2

--- osa4/index.js
+++ osa4/index.js
@@ -1,54 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
-
-logger.info('connecting to', config.MONGODB_URI)
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
