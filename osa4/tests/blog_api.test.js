3147afb048015ac88d2074782aa69b061c1af442
laurelcrelia <laurel.matt@helsinki.fi>
2024-06-30 15:05:27 +0300

    4.10

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -9,7 +9,7 @@ const helper = require('./test_helper')
 const Blog = require('../models/blog')
 const { blogsInDb } = require('./test_helper')
 
-describe('blog api', () => {
+describe('blog GET api', () => {
   beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.initialBlogs)
@@ -28,13 +28,37 @@ describe('blog api', () => {
     assert.strictEqual(response.body.length, 2)
   })
 
-  test('returned blogs have an id field', async () => {
+  test('returns blogs that have an id field', async () => {
     const response = await api.get('/api/blogs')
     const allHaveIdField = response.body.map(e => e.id !== undefined)
     const amountOfIdFields = blogsInDb.length
     for (let i = 0; i < amountOfIdFields; i++) {
       assert.strictEqual(allHaveIdField[i], true)
     }
+  })
+})
+
+describe('blog POST api', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+  test('can add a valid blog', async () => {
+    const newBlog = {
+      title: 'Testing is straightforward!',
+      author: 'Marja Meikalainen',
+      url: 'www.marjantestiblogi.fi',
+      likes: 3
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+    assert(titles.includes('Testing is straightforward!'))
   })
 })
 
